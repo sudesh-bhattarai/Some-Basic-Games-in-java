015c3b7b6d91271b3315cd4ccbfba8ad1fd55193
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-08-03 22:43:36 +0545

    Coin Toss Game

---
+++ CoinTossGame.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class CoinTossGame {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int score = 0;
+        char again;
+
+        do {
+            System.out.println("\n=== Coin Toss Game ===");
+            System.out.print("Guess (Heads/Tails): ");
+            String guess = sc.next();
+
+            String result = random.nextBoolean() ? "Heads" : "Tails";
+
+            System.out.println("Coin landed on: " + result);
+
+            if (guess.equalsIgnoreCase(result)) {
+                System.out.println("Correct Guess!");
+                score++;
+            } else {
+                System.out.println("Wrong Guess!");
+            }
+
+            System.out.println("Score: " + score);
+
+            System.out.print("Play again? (Y/N): ");
+            again = sc.next().charAt(0);
+
+        } while (again == 'Y' || again == 'y');
+
+        System.out.println("Final Score: " + score);
+        sc.close();
+    }
+}
